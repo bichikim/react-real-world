d67c5f3a846ea10ced8d2cf12aff58c1083183fa
Bichi Kim <bichi@live.co.kr>
2020-11-08 15:27:50 +0900

    fix httpEquiv name in CommonHead.ts

--- packages/web/src/components/CommonHead.ts
+++ packages/web/src/components/CommonHead.ts
@@ -5,7 +5,7 @@ export const CommonHead = () => {
   return (
     h(Head, null,
       h('meta', {charSet: 'utf-8'}),
-      h('meta', {content: 'IE=edge', 'http-equiv': 'X-UA-Compatible'}),
+      h('meta', {content: 'IE=edge', httpEquiv: 'X-UA-Compatible'}),
       h('meta', {content: 'width=device-width,initial-scale=1', name: 'viewport'}),
       h('meta', {content: '#4DBA87', name: 'theme-color'}),
       h('link', {href: '/manifest.json', rel: 'manifest'}),
