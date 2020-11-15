ed1d14ed4e4644155927dc8c64c7db5a37d0fb47
Bichi Kim <bichi@live.co.kr>
2020-11-15 22:01:39 +0900

    add ThenArg types

--- packages/web/src/types/language.ts
+++ packages/web/src/types/language.ts
@@ -8,3 +8,5 @@ export type  AnyFunction = (...args: any[]) => any
 export type AnyObject<T = any> = Record<number | string | symbol, T>
 
 export type PureObject<T = any> = Record<string, T>
+
+export type ThenArg<T> = T extends PromiseLike<infer U> ? U : T
