ba7ae36e71bc30c6651094132807a76c43777228
Bichi Kim <bichi@live.co.kr>
2020-11-18 09:57:36 +0900

    add lang setting & using middleware

--- packages/web/src/pages/_document.ts
+++ packages/web/src/pages/_document.ts
@@ -1,6 +1,14 @@
-import Document, {DocumentProps, Head, Html, Main, NextScript} from 'next/document'
+import Document, {DocumentProps, Html, Main, NextScript} from 'next/document'
 import {Fragment, createElement as h} from 'react'
-import {extractCritical} from 'emotion-server'
+import {extractCritical} from '@emotion/server'
+import * as middlewares from 'src/middlewares'
+import {middleware as executeMiddleware} from 'src/middleware'
+
+const marginPageProps = (props, pageProps = (props) => (props?.pageProps ?? {})) => {
+  const _pageProps = pageProps(props)
+  Object.assign(props['__NEXT_DATA__'].props.pageProps, _pageProps)
+  return props['__NEXT_DATA__'].props.pageProps
+}
 
 export interface Props extends DocumentProps {
   pageProps: any
@@ -10,9 +18,12 @@ export interface Props extends DocumentProps {
 class CustomDocument extends Document<Props> {
   static async getInitialProps(context) {
     const initialProps = await Document.getInitialProps(context)
+    const pageProps = await executeMiddleware(middlewares, context)
+
     const styles = extractCritical(initialProps.html)
     return {
       ...initialProps,
+      pageProps,
       styles: (h(Fragment, null,
         initialProps.styles,
         h('style', {
@@ -24,9 +35,12 @@ class CustomDocument extends Document<Props> {
   }
 
   render() {
+    const pageProps = marginPageProps(this.props)
+
+    const lang = pageProps?.language ?? 'ko'
+
     return (
-      h(Html, {lang: 'ko'},
-        h(Head),
+      h(Html, {lang},
         h('body', null,
           h(Main),
           h(NextScript),
