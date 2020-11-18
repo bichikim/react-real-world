6145e343623e068295ae69352a2db5863510c772
Bichi Kim <bichi@live.co.kr>
2020-11-18 09:56:22 +0900

    update supporting type and src

--- packages/web/src/components/Box.ts
+++ packages/web/src/components/Box.ts
@@ -5,24 +5,35 @@ import {useDebounce, withFastMemo} from 'src/hooks'
 import {
   color,
   ColorProps,
-  flexbox,
-  FlexProps,
+  flexbox, FlexboxProps,
   grid,
   GridProps,
   layout,
   LayoutProps,
   position,
   PositionProps,
+  ResponsiveValue,
   shadow,
   ShadowProps,
   space,
   SpaceProps,
+  style,
   typography,
   TypographyProps,
 } from 'styled-system'
+import {createShouldForwardProp, props} from '@styled-system/should-forward-prop'
 
-const StyledBox: FC<PureObject> = styled('div')(
-  color, space, layout, position, shadow, flexbox, grid, typography,
+interface TransformProps {
+  transform?: ResponsiveValue<string>
+}
+
+const transform = style({
+  cssProperty: 'transform',
+  prop: 'transform',
+})
+
+const StyledBox: FC<PureObject> = styled('div', {shouldForwardProp: createShouldForwardProp([...props, 'transform'])})(
+  color, space, layout, position, shadow, flexbox, grid, typography, transform,
 )
 
 export interface ASProps {
@@ -32,6 +43,11 @@ export interface ASProps {
 export interface HtmlProps {
   disabled?: boolean
   onClick?: () => any
+  type?: string
+}
+
+export interface ImgProps {
+  src?: string
 }
 
 export interface HtmlInputProps<S = any> {
@@ -41,7 +57,7 @@ export interface HtmlInputProps<S = any> {
 }
 
 export interface BoxProps<S = any> extends ColorProps, SpaceProps, LayoutProps, PositionProps, ShadowProps,
-  FlexProps, GridProps, TypographyProps, HtmlInputProps<S>, HtmlProps, ASProps {
+  FlexboxProps, GridProps, TypographyProps, HtmlInputProps<S>, HtmlProps, ASProps, ImgProps, TransformProps {
 }
 
 const ASYNC_WAIT = 45
@@ -50,19 +66,26 @@ const useAsyncEvent = (onChange, getter?: (value: any) => any, wait:number = ASY
   return useDebounce(onChange, wait, {getter})
 }
 
-const useChange = (onChange) => {
+const useChange = (onChange, type) => {
   return useCallback((event) => {
-    onChange && onChange(event?.target?.value)
-  }, [onChange])
+    switch (type) {
+      case 'checkbox':
+        return onChange && onChange(event?.target?.checked)
+      case 'radio':
+        return onChange && onChange(event?.target?.checked)
+      default:
+        return onChange && onChange(event?.target?.value)
+    }
+  }, [onChange, type])
 }
 
 export const Box: FC<BoxProps> = withFastMemo((props) => {
-  const {children, value = '', ...rest} = props
+  const {children, value = '', type, ...rest} = props
 
   const onClick = useAsyncEvent(props.onClick)
-  const onChange = useChange(props.onChange)
+  const onChange = useChange(props.onChange, type)
 
   return (
-    h(StyledBox, {...rest, onChange, onClick, value}, children)
+    h(StyledBox, {...rest, onChange, onClick, type, value}, children)
   )
 })
