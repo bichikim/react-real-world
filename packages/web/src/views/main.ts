849328db31d4dcce86f93682f7263146616961b2
Bichi Kim <bichi@live.co.kr>
2020-11-20 11:23:38 +0900

    update main page
    go product button position

--- packages/web/src/views/main.ts
+++ packages/web/src/views/main.ts
@@ -1,16 +1,28 @@
-import {FC, createElement as h, useCallback} from 'react'
-import {EmptyObject} from 'src/types'
-import {Box} from 'src/components/Box'
 import {useRouter} from 'next/router'
+import {FC, createElement as h, useCallback} from 'react'
+import {Box} from 'src/components/Box'
+import {EmptyObject} from 'src/types'
 
-export const MainPage:FC<EmptyObject> = () => {
-
+export const MainPage: FC<EmptyObject> = () => {
   const {push} = useRouter()
 
-  const goProducts = useCallback(() => push('/products').then(() => {
-    window.scrollTo(0, 0)
-  }), [push])
+  const goProducts = useCallback(() => {
+    return push('/products').then(() => {
+      window.scrollTo(0, 0)
+    })
+  }, [push])
 
-  return h(Box, {cursor: 'pointer', onClick: goProducts}, 'Go Products')
+  return h(Box, {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh',
+    justifyContent: 'center',
+    width: '100%',
+  }, h(Box, {
+    cursor: 'pointer',
+    fontSize: 'xl',
+    onClick: goProducts,
+  }, 'Go Products ✨',
+  ))
 }
 
