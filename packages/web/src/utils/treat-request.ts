15110f9adec59b72cfe9488d603010d0d69b971c
Bichi Kim <bichi@live.co.kr>
2020-11-20 10:43:22 +0900

    update treat-request.ts
    update the start function can modify args

--- packages/web/src/utils/treat-request.ts
+++ packages/web/src/utils/treat-request.ts
@@ -3,7 +3,7 @@ import {AnyFunction, ThenArg} from 'src/types'
 interface TreatRequestOptions<T extends AnyFunction> {
   done?: (value: ThenArg<ReturnType<T>>) => any
   error?: (error) => any
-  start?: (...args: Parameters<T>) => any
+  start?: (...args: Parameters<T>) => any[]
   trowingError?: boolean
 }
 
@@ -13,8 +13,8 @@ export const treatRequest = <T extends AnyFunction>(
 ): ((...args: Parameters<T>) => ReturnType<T>) => {
   const {done, error, start, trowingError = false} = options
   return (...args: Parameters<T>): ReturnType<T> => {
-    start && start(...args)
-    const promise = request(...args)
+    const newArgs = start?.(...args) ?? args
+    const promise = request(...newArgs)
 
     return promise.then((value) => {
       // eslint-disable-next-line promise/no-callback-in-promise
