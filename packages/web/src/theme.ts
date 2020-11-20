e8193f35474f0ee8e3ae3054136fe233e0345a65
Bichi Kim <bichi@live.co.kr>
2020-11-20 10:41:32 +0900

    add fontSize theme

--- packages/web/src/theme.ts
+++ packages/web/src/theme.ts
@@ -1,3 +1,5 @@
-export const defaultTheme = {
+import {Theme} from 'styled-system'
 
+export const defaultTheme: Theme = {
+  fontSizes: Object.assign([], {lg: '24px', md: '20px', sm: '16px', xl: '28px'}),
 }
