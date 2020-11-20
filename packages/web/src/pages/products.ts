4b725fb81fd9b69dbe3bf13eba238b2c414531b7
Bichi Kim <bichi@live.co.kr>
2020-11-20 11:16:41 +0900

    update products.ts
    add layout in a products page

--- packages/web/src/pages/products.ts
+++ packages/web/src/pages/products.ts
@@ -1,7 +1,20 @@
 import {ProductsPage} from 'src/views/products'
 import {withMiddlewares} from 'src/with-middlewares'
+import {withLayout} from 'src/hooks'
+import {DefaultLayout} from 'src/layout/default'
+import {products} from 'src/store/products'
 
-export default ProductsPage
+export default withLayout(DefaultLayout)(ProductsPage)
 
-export const getServerSideProps = withMiddlewares()
+export const getServerSideProps = withMiddlewares(async () => {
+  await products.requestGetProducts({offset: 0})
+
+  return {
+    props: {
+      offset: products.state.offset,
+      products: [...products.state.products.values()],
+      timestamp: products.state.timestamp,
+    },
+  }
+})
 
