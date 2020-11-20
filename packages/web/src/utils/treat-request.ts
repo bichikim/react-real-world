ef12584213b46d892419ac58d2d8cbc630498aae
Bichi Kim <bichi@live.co.kr>
2020-11-20 11:15:16 +0900

    update treat-request.ts
    start can return void

--- packages/web/src/utils/treat-request.ts
+++ packages/web/src/utils/treat-request.ts
@@ -3,7 +3,7 @@ import {AnyFunction, ThenArg} from 'src/types'
 interface TreatRequestOptions<T extends AnyFunction> {
   done?: (value: ThenArg<ReturnType<T>>) => any
   error?: (error) => any
-  start?: (...args: Parameters<T>) => any[]
+  start?: (...args: Parameters<T>) => any[] | void
   trowingError?: boolean
 }
 
@@ -13,8 +13,9 @@ export const treatRequest = <T extends AnyFunction>(
 ): ((...args: Parameters<T>) => ReturnType<T>) => {
   const {done, error, start, trowingError = false} = options
   return (...args: Parameters<T>): ReturnType<T> => {
-    const newArgs = start?.(...args) ?? args
-    const promise = request(...newArgs)
+    const newArgs = start?.(...args)
+
+    const promise = request(...(newArgs || args))
 
     return promise.then((value) => {
       // eslint-disable-next-line promise/no-callback-in-promise
