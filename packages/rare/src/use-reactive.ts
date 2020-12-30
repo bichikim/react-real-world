490e73fd76074798d72915f5cad26549bf900da9
Bichi Kim <bichi@live.co.kr>
2020-12-31 02:23:18 +0900

    update rare

--- packages/rare/src/use-reactive.ts
+++ packages/rare/src/use-reactive.ts
@@ -1,36 +1,41 @@
 import {useCallback, useEffect, useRef, useState} from 'react'
-import {Recipe, Store} from './store'
+import {Recipe, Store} from 'src/Store'
 
 export type Actor = (...args: any[]) => Promise<any> | any
 
+// export type UseReactiveReturnType<S> = [S, (recipe: Recipe<S>) => any]
+
 export const useReactive = <S>(store: Store<S>, initState?: Partial<S>): [S, (recipe: Recipe<S>) => any] => {
   const [_state, _setState] = useState(store.state)
-  const mutate = useCallback((draft: Recipe<S>) => {
-    store.mutate(draft)
+
+  const dispatch = useCallback((recipe) => {
+    store.dispatch(recipe, 'dispatch')
   }, [store])
+
   const [savedStore] = useState(store)
+
   const ones = useRef(true)
 
   if (ones.current) {
     ones.current = false
 
-    savedStore.registerHooks(_setState)
-    savedStore.registerHooks(_setState)
+    savedStore.registerDispatcher(_setState)
 
     if (initState) {
-      store.mutate((draft) => {
+
+      store.dispatch((draft) => {
         Object.assign(draft, initState)
-      }, _setState)
+      }, 'init')
     }
   }
 
   useEffect(() => {
     return () => {
-      savedStore.unRegisterHooks(_setState)
+      savedStore.unRegisterDispatcher(_setState)
     }
   }, [savedStore])
 
-  return [_state, mutate]
+  return [_state, dispatch]
 }
 
 export const createReactive = <S>(store: Store<S>) => (): [S, (recipe: Recipe<S>) => any] => useReactive(store)
